c9ab9fbda3c270c0fcecacffea51f66be7f62b80
Hassan Yakubu <yhassan677@gmail.com>
2024-11-26 22:31:48 +0000

    update

--- db/restaurant_db.sql
+++ db/restaurant_db.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 22, 2024 at 01:18 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Nov 26, 2024 at 11:30 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,11 +43,18 @@ CREATE TABLE `admin_logs` (
 CREATE TABLE `feedback` (
   `feedback_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `item_id` int(11) DEFAULT NULL,
+  `category` enum('Customer Service','Food Quality','Pool Table') NOT NULL,
   `rating` int(11) DEFAULT NULL CHECK (`rating` between 1 and 5),
   `comments` text DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedback_id`, `user_id`, `category`, `rating`, `comments`, `created_at`) VALUES
+(1, 1, 'Customer Service', 1, 'Bad', '2024-11-26 21:00:49');
 
 -- --------------------------------------------------------
 
@@ -65,6 +72,28 @@ CREATE TABLE `menu_items` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `menu_items`
+--
+
+INSERT INTO `menu_items` (`item_id`, `name`, `description`, `price`, `category`, `availability`, `created_at`) VALUES
+(1, 'Plain Rice', 'White rice served with tomato sauce.', 28.00, 'Lunch', 1, '2024-11-26 21:31:13'),
+(2, 'Red Red', 'Fried ripe plantain with beans stew.', 30.00, 'Lunch', 1, '2024-11-26 21:33:00'),
+(3, 'Waakye', 'Waakye served with spaghetti and tomato sauce.', 28.00, 'Breakfast', 1, '2024-11-26 21:33:48'),
+(4, 'Bread', 'White bread', 5.00, 'Breakfast', 1, '2024-11-26 21:35:45'),
+(5, 'Sausage', 'Fried sausages', 8.00, 'Breakfast', 1, '2024-11-26 21:36:03'),
+(6, 'Egg', 'Fried eggs', 8.00, 'Breakfast', 1, '2024-11-26 21:36:18'),
+(7, 'Milo', 'Warm chocolate drink', 10.00, 'Drink', 1, '2024-11-26 21:37:17'),
+(8, 'Nescafe', 'Coffee', 10.00, 'Drink', 1, '2024-11-26 21:38:10'),
+(9, 'Coke', 'Can coke', 12.00, 'Drink', 1, '2024-11-26 21:39:00'),
+(10, 'Hibiscus Fusion', 'Sobolo', 10.00, 'Drink', 1, '2024-11-26 21:39:24'),
+(11, 'Blue Skies', 'Mango and Passion Fruit', 25.00, 'Drink', 1, '2024-11-26 21:39:53'),
+(12, 'Spaghetti', 'Served with beef/sauteed chicked', 28.00, 'Lunch', 1, '2024-11-26 21:41:39'),
+(13, 'Pancakes', 'Fluffy pancakes', 8.00, 'Breakfast', 1, '2024-11-26 21:42:31'),
+(14, 'Fufu', 'Fufu served with soup of your choice', 28.00, 'Lunch', 1, '2024-11-26 21:43:09'),
+(15, 'Banku', 'Banku served with tilapia', 28.00, 'Lunch', 1, '2024-11-26 21:46:38'),
+(16, 'Beef Burger', 'Burger served with french fries', 55.00, 'Dinner', 1, '2024-11-26 21:47:25');
+
 -- --------------------------------------------------------
 
 --
@@ -80,6 +109,16 @@ CREATE TABLE `orders` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `total_price`, `status`, `payment_method`, `created_at`) VALUES
+(1, 1, 56.00, 'pending', 'cash', '2024-11-26 21:48:58'),
+(2, 2, 84.00, 'pending', 'cash', '2024-11-26 21:54:30'),
+(3, 2, 84.00, 'pending', 'cash', '2024-11-26 21:55:05'),
+(4, 2, 40.00, 'pending', 'card', '2024-11-26 21:56:15');
+
 -- --------------------------------------------------------
 
 --
@@ -93,6 +132,16 @@ CREATE TABLE `order_details` (
   `quantity` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`detail_id`, `order_id`, `item_id`, `quantity`, `price`) VALUES
+(1, 1, 1, 2, 28.00),
+(2, 2, 3, 3, 28.00),
+(3, 3, 15, 3, 28.00),
+(4, 4, 13, 5, 8.00);
 
 -- --------------------------------------------------------
 
@@ -126,6 +175,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `fname`, `lname`, `email`, `password`, `role`, `created_at`) VALUES
+(1, 'Hassan', 'Yakubu', 'yhassan677@gmail.com', '$2y$10$ZkxVoXtf98/IIU2Itp5EJu3MaLo6rKPgrjqaqFs.Ab9M4Rgv7krEC', 'admin', '2024-11-26 20:54:26'),
+(2, 'Victor', 'Quagraine', 'vquagraine@gmail.com', '$2y$10$h7oBonICe9sMUt65RE1E5.S0P2itSQVzT8.3JMvpkcPNEV3wEG1cm', 'customer', '2024-11-26 21:52:21');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -141,8 +198,7 @@ ALTER TABLE `admin_logs`
 --
 ALTER TABLE `feedback`
   ADD PRIMARY KEY (`feedback_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `item_id` (`item_id`);
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `menu_items`
@@ -193,25 +249,25 @@ ALTER TABLE `admin_logs`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `menu_items`
 --
 ALTER TABLE `menu_items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `reservations`
@@ -223,7 +279,7 @@ ALTER TABLE `reservations`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -239,8 +295,7 @@ ALTER TABLE `admin_logs`
 -- Constraints for table `feedback`
 --
 ALTER TABLE `feedback`
-  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `menu_items` (`item_id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `orders`
@@ -265,13 +320,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-ALTER TABLE feedback
-DROP FOREIGN KEY feedback_ibfk_2;
-
-ALTER TABLE feedback
-DROP COLUMN item_id;
-
-ALTER TABLE feedback
-ADD category ENUM('Customer Service', 'Food Quality', 'Pool Table') NOT NULL AFTER user_id;
